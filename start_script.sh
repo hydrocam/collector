dd6f5ee14cd53adc4700900bb66a3cdd2e60ce57
Sajan Neupane <158505701+10sajan10@users.noreply.github.com>
2024-09-29 12:23:39 -0600

    Create start_script.sh

---
+++ start_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /path/to/your/virtualenv/folder/bin/activate
+
+# Change to the cloned repository directory
+cd /path/to/your/collector
+
+# Execute the main Python script
+python3 src/main.py
